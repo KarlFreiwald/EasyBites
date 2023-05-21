68527464977f4588d4b55fc0296797078e662267
Peter Breitzler <peter.breitzler@continental-corporation.com>
2023-05-21 08:54:17 +0200

    Changed naming of single recipe view

---
+++ controllers/recipes.js
@@ -0,0 +1,21 @@
+const logger = require('../utils/logger');
+
+const recipes = {
+    index(request, response) {
+        const viewData = {
+            title: 'Recipes',
+        };
+
+        response.render('recipes.hbs', viewData);
+    },
+    view(request, response) {
+        const viewData = {
+            title: 'Recipe', // TODO: change to recipes name
+            recipe_id: request.params.recipe_id
+        };
+
+        response.render('recipe-view.hbs', viewData);
+    }
+}
+
+module.exports = recipes;
--- routes.js
+++ routes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 
 const home = require('./controllers/home.js');
 const about = require('./controllers/about.js');
-const recipe = require('./controllers/recipe.js');
+const recipes = require('./controllers/recipes.js');
 
 router.get('/', home.index);
 router.get('/about', about.index);
-router.get('/recipe/:recipe_id', recipe.index);
+router.get('/recipes', recipes.index);
+router.get('/recipes/:recipe_id', recipes.view);
 
 module.exports = router;
