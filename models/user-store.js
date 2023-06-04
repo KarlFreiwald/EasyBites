f31d9ab9471a70f4dad4d90ea2c3528dd2aebb24
Peter Breitzler <peter.breitzler@continental-corporation.com>
2023-06-04 17:03:14 +0200

    Changes for new db structure

--- models/user-store.js
+++ models/user-store.js
@@ -3,24 +3,25 @@ const logger = require('../utils/logger.js');
 
 const userStore = {
     async addUser(user) {
-        logger.info(`addUser: ${JSON.stringify(user)}`);
+        logger.info(`Create user ${user.email}`);
 
         try {
             await dataStore.query(
-                'INSERT INTO users (email, password) VALUES($1, $2)',
+                'INSERT INTO eb_user (email, password) VALUES($1, $2)',
                 [user.email, user.password],
                 'Error registering user',
                 true
             );
 
-            return true
+            return true;
         } catch (e) {
             return false;
         }
     },
     async authenticateUser(email, password) {
+        logger.info(`Authenticate user ${email}`)
         const dbRes = await dataStore.query(
-            'SELECT * FROM users WHERE email=$1 AND password=$2',
+            'SELECT * FROM eb_user WHERE email=$1 AND password=$2',
             [email, password],
             'error finding user from email and password',
         );
